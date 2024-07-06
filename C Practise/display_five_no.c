db4b12a954d66aec7172c5cac25169f5c1cc18bd
AkkalDhami <dhamiakkal21@gmail.com>
2024-07-06 14:00:58 +0545

    Update display_five_no.c

--- C Practise/display_five_no.c
+++ C Practise/display_five_no.c
@@ -2,26 +2,17 @@
 
 int main()
 {
-    // int a[5];
-    // int i;
-    // for (i = 0; i < 5; i++)
-    // {
-    // printf("Enter the 5 elements of array\n");
-    //     scanf("%d", &a[i]);
-    // }
-    // printf("The elements of array are\n");
-    // for (i = 0; i < 5; i++)
-    // {
-    //     printf("%d\n", a[i]);
-    // }
-    int i, n;
-    printf("\nEnter the number: ");
-    scanf("%d", &n);
-
-    printf("Multiplication of %d \n", n);
-    for ( i = 1; i <=10; i++)
+    int a[5];
+    int i;
+    for (i = 0; i < 5; i++)
     {
-       printf("%d * %d = %d\n", n, i, n*i);
+    printf("Enter the 5 elements of array\n");
+        scanf("%d", &a[i]);
+    }
+    printf("The elements of array are\n");
+    for (i = 0; i < 5; i++)
+    {
+        printf("%d\n", a[i]);
     }
     return 0;
 }
