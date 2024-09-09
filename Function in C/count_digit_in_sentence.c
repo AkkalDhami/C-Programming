31db1248e0b4dc73732f22dae943f4565a3b5df4
Aavash Dhami <dhamiakkal21@gmail.com>
2024-09-09 13:02:34 +0545

    Add files via upload

---
+++ Function in C/count_digit_in_sentence.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+void findDigit(char str[])
+{
+
+    int count = 0;
+    int digit = 0;
+
+    printf("Digits Are : ");
+    for (int i = 0; i < str[i] != '\0'; i++)
+    {
+        if (str[i] >= '0' && str[i] <= '9')
+        {
+            digit++;
+            count += (str[i] - '0');
+            printf("%d ", str[i] - '0');
+        }
+    }
+    if (digit == 0)
+    {
+        printf("No digits found in the string.\n");
+    }
+    printf("\nNumber of digits: %d\n", digit);
+}
+int main()
+
+{
+    char str[100];
+    printf("Enter a string: ");
+    scanf("%[^\n]%*c", str);
+    findDigit(str);
+    return 0;
+}
