270f1fae5b88d85acdc51724c29cf53dfe9d8a90
Aavash Dhami <dhamiakkal21@gmail.com>
2024-09-11 15:55:15 +0545

    Update series_odd_no.c

--- Function in C/series_odd_no.c
+++ Function in C/series_odd_no.c
@@ -1,17 +1,15 @@
 #include <stdio.h>
 
-int printSeries(int n)
+void printSeries(int n)
 {
     int i;
     for (i = 3; i <= n; i++)
     {
         if (i % 3 == 0)
         {
-
             printf("%d ", i);
         }
     }
-    return 0;
 }
 int main()
 {
