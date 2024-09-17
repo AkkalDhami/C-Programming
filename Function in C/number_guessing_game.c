562b6f30382e3e257cb098b6a2dc30b61e263122
Aavash Dhami <dhamiakkal21@gmail.com>
2024-09-17 20:51:46 +0545

    number_guessing_game.c

--- Function in C/number_guessing_game.c
+++ Function in C/number_guessing_game.c
@@ -1,92 +1,35 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <time.h>
-
-void guess(int N)
+#include <stdlib.h>
+int main()
 {
-    int number, guess, numberofguess = 0;
-<<<<<<< Tabnine <<<<<<<
-    /**//+
-     * @brief This function allows the user to guess a randomly generated number within a specified range.//+
-     *//+
-     * @param N The upper limit of the range (inclusive) within which the number will be generated.//+
-     *//+
-     * @return void//+
-     *//+
-     * The function prompts the user to guess a number between 1 and N. It continues to prompt the user until they guess the correct number or reach 10 attempts.//+
-     * If the user guesses the correct number, it congratulates them and displays the number of attempts it took.//+
-     * If the user does not guess the correct number within 10 attempts, it informs them that they lost and displays the correct number.//+
-     *///+
-    void guess(int N)//+
-    {//+
-        int number, guess, numberofguess = 0;//+
-        srand(time(NULL));//+
-        number = rand() % N;//+
-    //+
-        printf("Guess a number between 1 and %d: ", N);//+
-    //+
-        do//+
-        {//+
-            if (numberofguess > 9)//+
-            {//+
-                printf("\nYou lose! The correct number was %d.\n", number);//+
-                break;//+
-            }//+
-    //+
-            scanf("%d", &guess);//+
-    //+
-            if (guess > number)//+
-            {//+
-                printf("Lower number, please!\n");//+
-                numberofguess++;//+
-            }//+
-            else if (number > guess)//+
-            {//+
-                printf("Higher number, please!\n");//+
-                numberofguess++;//+
-            }//+
-            else//+
-            {//+
-                printf("Congratulations! You guessed the number in %d attempts.\n", numberofguess);//+
-            }//+
-        } while (guess != number);//+
-    }//+
->>>>>>> Tabnine >>>>>>>// {"conversationId":"c10f6a44-0aed-4934-ab29-62911017f2da","source":"instruct"}
-    srand(time(NULL));
-    number = rand() % N;
+    int number;
+    srand(time(0));
+    number = rand() % 100 + 1; // Generate a random number between 1 and 100
 
-    printf("Guess a number between 1 and %d: ", N);
+    int guess, noOfGuesses = 1;
 
     do
     {
-        if (numberofguess > 9)
+        printf("Guess the number(1-100):");
+        scanf("%d", &guess);
+        if (guess > number)
+        {
+            printf("Lower Number Please!\n");
+        }
+        else if (number > guess)
+            printf("Higher Number Please!\n");
+
+        else
+            printf("You guessed in %d attempts\n", noOfGuesses);
+
+        if (noOfGuesses > 10)
         {
             printf("\nYou lose! The correct number was %d.\n", number);
             break;
         }
 
-        scanf("%d", &guess);
-
-        if (guess > number)
-        {
-            printf("Lower number, please!\n");
-            numberofguess++;
-        }
-        else if (number > guess)
-        {
-            printf("Higher number, please!\n");
-            numberofguess++;
-        }
-        else
-        {
-            printf("Congratulations! You guessed the number in %d attempts.\n", numberofguess);
-        }
+        noOfGuesses++;
     } while (guess != number);
-}
-
-int main()
-{
-    int N = 20;
-    guess(N);
     return 0;
 }
