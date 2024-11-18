01522e77ad09fbfe6f6547b0460db9b1acf59418
Aavash Dhami <dhamiakkal21@gmail.com>
2024-11-18 16:30:40 +0545

    Update read_write_details.c

--- File Handling in C/read_write_details.c
+++ File Handling in C/read_write_details.c
@@ -13,7 +13,6 @@ void writeToFile(int n)
     FILE *file;
     struct Student student;
 
-    // Open the file in write mode
     file = fopen("test.txt", "w");
     if (file == NULL)
     {
@@ -21,7 +20,6 @@ void writeToFile(int n)
         exit(1);
     }
 
-    // Get details from the user and write to the file
     for (int i = 0; i < n; i++)
     {
         printf("\nEnter details for student %d:\n", i + 1);
@@ -32,11 +30,9 @@ void writeToFile(int n)
         printf("Marks: ");
         scanf("%f", &student.marks);
 
-        // Write to file
         fprintf(file, "%d %s %.2f\n", student.rollNo, student.name, student.marks);
     }
 
-    // Close the file
     fclose(file);
 }
 
@@ -45,7 +41,6 @@ void readFromFile()
     FILE *file;
     struct Student student;
 
-    // Open the file in read mode
     file = fopen("test.txt", "r");
     if (file == NULL)
     {
@@ -57,13 +52,11 @@ void readFromFile()
     printf("Roll No\tName\t\tMarks\n");
     printf("-----------------------------------\n");
 
-    // Read and display student details from the file
     while (fscanf(file, "%d %49s %f", &student.rollNo, student.name, &student.marks) == 3)
     {
         printf("%d\t%-15s%.2f\n", student.rollNo, student.name, student.marks);
     }
 
-    // Close the file
     fclose(file);
 }
 
@@ -71,14 +64,10 @@ int main()
 {
     int n;
 
-    // Ask user for the number of students
     printf("Enter the number of students: ");
     scanf("%d", &n);
 
-    // Write student details to file
     writeToFile(n);
-
-    // Read and display student details from file
     readFromFile();
 
     return 0;
