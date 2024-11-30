75c5658c0b878fb30a956597152b15db876f1211
Aavash Dhami <dhamiakkal21@gmail.com>
2024-11-30 17:56:06 +0545

    Update 2_stud_details.c

--- Revision/File Handling/2_stud_details.c
+++ Revision/File Handling/2_stud_details.c
@@ -2,50 +2,46 @@
 
 int main()
 {
-    FILE *file;
-    file = fopen("student.txt", "w");
+    FILE *fptr;
 
+    // Open file in write mode
+    fptr = fopen("student.txt", "w");
+
+    int rollno, marks1, marks2, marks3;
     char name[50];
-    int roll, sub[3];
 
     for (int i = 0; i < 5; i++)
     {
-        printf("Enter roll number: ");
-        scanf("%d", &roll);
-        printf("Enter name: ");
+        printf("Enter roll no: ");
+        scanf("%d", &rollno);
+        printf("Enter your name: ");
         scanf("%s", name);
-        printf("Enter marks in three subjects: ");
-        for (int j = 0; j < 3; j++)
-        {
-            scanf("%d", &sub[j]);
-        }
+        printf("Enter marks 1: ");
+        scanf("%d", &marks1);
+        printf("Enter marks 2: ");
+        scanf("%d", &marks2);
+        printf("Enter marks 3: ");
+        scanf("%d", &marks3);
+
+        fprintf(fptr, "%d %s %d %d %d\n", rollno, name, marks1, marks2, marks3);
     }
 
-    for (int i = 0; i < 5; i++)
+    fclose(fptr);
+
+    fptr = fopen("student.txt", "r");
+    if (fptr == NULL)
     {
-        fprintf(file, "\nStudent-%d Details\n", i + 1);
-        fprintf(file, "Roll No: %d\nName: %s\nMarks in Subject1: %d\nMarks in Subject2: %d\nMarks in Subject3: %d\n", roll, name, sub[0], sub[1], sub[2]);
+        printf("Error! Could not open file for reading.\n");
+        return 1;
     }
-    fclose(file);
 
-    file = fopen("student.txt", "r");
-
-    if (file == NULL)
+    printf("\nStudent Details from File:\n");
+    while (fscanf(fptr, "%d %s %d %d %d", &rollno, name, &marks1, &marks2, &marks3) == 5)
     {
-        printf("Could not open file \n");
-    }
-    else
-    {
-        for (int i = 0; i < 5; i++)
-        {
-            fscanf(file, "%d\n%s\n%d\n %d\n%d\n", &roll, name, &sub[0], &sub[1], &sub[2]);
-            printf("\nStudent-%d Details\n", i + 1);
-            printf("Roll No: %d\nName: %s\nMarks in Subject1: %d\nMarks in Subject2: %d\nMarks in Subject3: %d\n", roll, name, sub[0], sub[1], sub[2]);
-        }
-
-        fclose(file);
-        printf("\nProgram is executed by Akkal Dhami\n");
+        printf("\nRoll No: %d\nName: %s\nMarks 1: %d\nMarks 2: %d\nMarks 3: %d\n", rollno, name, marks1, marks2, marks3);
     }
 
+    fclose(fptr);
+    printf("\nProgram is executed by Akkal Dhami\n");
     return 0;
 }
