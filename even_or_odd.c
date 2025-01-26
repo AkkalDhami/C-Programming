ecec7a485816e761cefce09dbe05592ccf8525c5
Aavash Dhami <dhamiakkal21@gmail.com>
2025-01-26 21:48:13 +0545

    Update even_or_odd.c

--- even_or_odd.c
+++ even_or_odd.c
@@ -1,25 +1,18 @@
 #include <stdio.h>
 int main()
 {
-    // int n;
-    // printf("Enter a number:");
-    // scanf("%d", &n);
+     int n;
+     printf("Enter a number:");
+     scanf("%d", &n);
 
     // n  % 2 ==  0 ? printf("The number is even\n") : printf("The number is odd\n");
-
-    // if (n % 2 == 0)
-    // {
-    //     printf("%d is even number", n);
-    // }
-
-    // else
-    //     printf("%d is odd number", n);
-
-    int n, fact = 1;
-    for(int i =1; i=4; i++){
-        fact = fact * i/n;
-        printf("Factorial = %d", fact);
-    }
+    //OR,
+     if (n % 2 == 0)
+     {
+         printf("%d is even number", n);
+     }
+     else
+        printf("%d is odd number", n);
 
     return 0;
 }
