0040c7c5919b8b5db7661e2a5799582a25b4284d
Aavash Dhami <dhamiakkal21@gmail.com>
2025-02-18 23:09:58 +0545

    bank account details

---
+++ Revision/Practise/18_bank_account.c
@@ -0,0 +1,60 @@
+//? WAP to read the account no, name and balance of 'n' number of customers and display the account no, name and balance of those customers whose balance is greater than 5000 and save the data in a file.
+
+#include <stdio.h>
+struct customer
+{
+    int acc_no;
+    char name[30];
+    float balance;
+};
+
+int main()
+{
+
+    int n;
+    FILE *fptr;
+    fptr = fopen("customer.dat", "w");
+    if (fptr == NULL)
+    {
+        printf("Error in Writing File!!!\n");
+        return 1;
+    }
+    printf("Enter the number of customers: ");
+    scanf("%d", &n);
+
+    struct customer c[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        printf("Enter the details of customer-%d\n", i + 1);
+        printf("Enter account no: ");
+        scanf("%d", &c[i].acc_no);
+        printf("Enter name: ");
+        scanf("%s", c[i].name);
+        printf("Enter balance: ");
+        scanf("%f", &c[i].balance);
+        fprintf(fptr, "%d %s %.2f\n", c[i].acc_no, c[i].name, c[i].balance);
+    }
+    fclose(fptr);
+
+    fptr = fopen("customer.dat", "r");
+    if (fptr == NULL)
+    {
+        printf("Error in Reading File!!!\n");
+        return 1;
+    }
+
+    printf("\nCustomers with balance greater than 5000:\n");
+    while (fscanf(fptr, "%d %s %f", &c[0].acc_no, c[0].name, &c[0].balance) != EOF)
+    {
+        if (c[0].balance > 5000)
+        {
+            printf("Account No: %d\n", c[0].acc_no);
+            printf("Name: %s\n", c[0].name);
+            printf("Balance: %.2f\n", c[0].balance);
+            printf("\n");
+        }
+    }
+
+    return 0;
+}
